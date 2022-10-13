0e3a62eb61b2a03a5c3b1d8f066278043f034aed
Enrique Buchán <106187397+ionbuc@users.noreply.github.com>
2022-10-13 13:25:25 -0500

    Update querly-mysql-cursor.py
    Programa para realizar la consulta

--- querly-mysql-cursor.py
+++ querly-mysql-cursor.py
@@ -1,1 +1,26 @@
+# Bibliotecas
+import mysql.connector
 
+# Conexion
+cnx = mysql.connector.connect(user='kikelan', 
+                              password='0123', 
+                              host='127.0.0.1',
+                              database='codigoIot')
+
+# Cursor
+cursor = cnx.cursor()
+
+# Query
+query = ("SELECT id,nombre,temperatura,fecha FROM clima WHERE id=555;")
+
+# Ejecutar cursor
+cursor.execute (query)
+
+res = cursor.fetchall ()
+
+for x in res:
+    print (x)
+
+# Cerrar todo
+cursor.close()
+cnx.close()
